f270a8dfbe0655400d55ff7eb61452ae9220579f
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-15 11:16:33 -0400

    Implement initialize method for Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class Board
+  WIDTH = 7
+  HEIGHT = 6
+
+  def initialize
+    @board = Array.new(WIDTH)
+
+    (0..WIDTH).each do |column|
+      @board[column] = []
+      (0..HEIGHT).each do |row|
+        @board[column][row] = nil
+      end
+    end
+  end
 end
