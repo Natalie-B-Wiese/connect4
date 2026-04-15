70b2834c9b42cbb56ed5fd339870550f1c9663ae
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-15 16:32:53 -0400

    Create player class

---
+++ lib/player.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# a basic data structure that represents a player
+class Player
+  @@player_count = 0
+
+  def initialize(name)
+    @id = @@player_count
+    @name = name
+    @@player_count += 1
+  end
+
+  def to_s
+    @name
+  end
+end
