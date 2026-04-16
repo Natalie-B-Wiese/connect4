54b0876ac8453c92604d38456c19d5fda90d82ab
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-16 12:02:14 -0400

    Implement to_column_number method
    to_column_number works because it passes all tests

--- lib/game.rb
+++ lib/game.rb
@@ -9,4 +9,19 @@ class Game
     @player2 = player2
     @board = Board.new
   end
+
+  # if input-1 is a valid column index it returns a 0-based column number from input
+  # Otherwise returns nil
+  # NOTE: it does not check if the column is full
+  def to_column_number(string_input)
+    # if must be an integer digit of any length that doesn't begin with 0
+    return nil unless string_input.match(/^[1-9]\d*$/)
+
+    int_input = string_input.to_i
+
+    # return the input if it is in a valid column rangge
+    return int_input - 1 if int_input >= 1 && int_input <= Board::WIDTH
+
+    nil
+  end
 end
