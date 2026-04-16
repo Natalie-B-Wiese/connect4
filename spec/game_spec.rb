caaae9c1df306b6374c2d801d80b861a55d40c44
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-16 11:18:05 -0400

    Write tests for Game to_column_number
    Now that the tests for to_column_number are written, the method to_column_number can be written and it can be assured it is working if it passes all the tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,4 +14,76 @@ describe Game do
       end
     end
   end
+
+  describe '#to_column_number' do
+    subject(:game_check) { described_class.new(player1, player2) }
+    # NOTE: #to_column_number will only return a value if input-1 is a valid column index, otherwise nil
+
+    context 'when given valid input' do
+      it 'returns input minus 1' do
+        user_input = '3'
+        goal = 2
+        result = game_check.to_column_number(user_input)
+        expect(result).to eq(goal)
+      end
+    end
+
+    context 'when given first column' do
+      it 'returns 0' do
+        user_input = '1'
+        goal = 0
+        result = game_check.to_column_number(user_input)
+        expect(result).to eq(goal)
+      end
+    end
+
+    context 'when given last column' do
+      it 'returns WIDTH minus 1' do
+        user_input = Board::WIDTH.to_s
+        goal = user_input.to_i - 1
+        result = game_check.to_column_number(user_input)
+        expect(result).to eq(goal)
+      end
+    end
+
+    context 'when given a column of 0' do
+      it 'returns nil' do
+        user_input = '0'
+        result = game_check.to_column_number(user_input)
+        expect(result).to be_nil
+      end
+    end
+
+    context 'when given a negative column' do
+      it 'returns nil' do
+        user_input = '-100'
+        result = game_check.to_column_number(user_input)
+        expect(result).to be_nil
+      end
+    end
+
+    context 'when given a column bigger than board width' do
+      it 'returns nil' do
+        user_input = (Board::WIDTH + 1).to_s
+        result = game_check.to_column_number(user_input)
+        expect(result).to be_nil
+      end
+    end
+
+    context 'when given invalid input' do
+      it 'returns nil' do
+        user_input = '5b'
+        result = game_check.to_column_number(user_input)
+        expect(result).to be_nil
+      end
+    end
+
+    context 'when given decimal' do
+      it 'returns nil' do
+        user_input = '1.24'
+        result = game_check.to_column_number(user_input)
+        expect(result).to be_nil
+      end
+    end
+  end
 end
